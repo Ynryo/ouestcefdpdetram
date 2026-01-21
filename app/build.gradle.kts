d8371f20e29febdcd41aa671af50957c90179e88
Ynryo <ewenatideklein@gmail.com>
2026-01-21 15:49:39 +0100

    add app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "fr.ynryo.ouestcefdpdetram"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
