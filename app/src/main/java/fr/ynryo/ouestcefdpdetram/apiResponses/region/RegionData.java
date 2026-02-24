68d87e20356badc572e98f0e31deebfa84a9ef21
Ynryo <ewenatideklein@gmail.com>
2026-02-24 10:38:05 +0100

    refactor: Simplify `RegionData` class
    Removed unused fields (`authority`, `logoHref`, `regionId`) and their corresponding getter methods.
    
    A constructor was added to initialize the `id` and `name` fields. The `toString()` method was also updated to reflect these changes.

--- app/src/main/java/fr/ynryo/ouestcefdpdetram/apiResponses/region/RegionData.java
+++ app/src/main/java/fr/ynryo/ouestcefdpdetram/apiResponses/region/RegionData.java
@@ -2,14 +2,14 @@ package fr.ynryo.ouestcefdpdetram.apiResponses.region;
 
 import androidx.annotation.NonNull;
 
-import java.net.URI;
-
 public class RegionData {
     private int id;
     private String name;
-    private String authority;
-    private URI logoHref;
-    private int regionId;
+
+    public RegionData(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
@@ -19,27 +19,12 @@ public class RegionData {
         return name;
     }
 
-    public String getAuthority() {
-        return authority;
-    }
-
-    public URI getLogoHref() {
-        return logoHref;
-    }
-
-    public int getRegionId() {
-        return regionId;
-    }
-
     @NonNull
     @Override
     public String toString() {
         return "RegionData{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", authority='" + authority + '\'' +
-                ", logoHref=" + logoHref +
-                ", regionId=" + regionId +
                 '}';
     }
 }
